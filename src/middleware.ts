d62059d6495d14a14ea6513ac81c5022862dbe30
Nishant A S <nishantas4043@gmail.com>
2024-06-08 19:29:46 +0530

    make assetlinks.json requrie no authentication

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 export { auth as default } from "@/lib/services/auth";
 export const config = {
-  matcher: ["/((?!api/auth|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!api/auth|_next/static|_next/image|favicon.ico|\.well-known/assetlinks.json).*)"],
 };
 
 export const runtime = "experimental-edge";
